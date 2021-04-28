07607ef016c4a789f4b0eb6df6916af1e6112add
Ali Faraji <billa.svpc@gmail.com>
2021-04-29 00:17:19 +0430

    basic features like fetch and showing data implement. todo: - animation when we want to show loaded data - loading and error message style - form lock when loading. - local storage for caching data

--- script.js
+++ script.js
@@ -11,3 +11,111 @@
                                |__/
 */
 
+
+async function getUserInfo(username) {
+
+    let responseObject = {
+        data: {},
+        success: true,
+        errorMessage: ""
+    }
+
+    return fetch("https://api.github.com/users/" + username).then(function (response) {
+
+        if (!response.ok) {
+
+            if (response.status == 404)
+                throw Error("User not found.")
+
+            throw Error("Error in getting user info.")
+        }
+
+        return response.json()
+
+    }).then(function (body) {
+        responseObject.data = body
+        return responseObject;
+
+    }).catch(function (error) {
+        responseObject.success = false;
+        responseObject.errorMessage = error.message;
+        return responseObject;
+    })
+
+}
+
+
+
+function showDetail(data) {
+
+    /**
+     * getting required info from json.
+     */
+    let name = data["name"];
+    let bio = data["bio"];
+    let location = ["location"];
+    let blog = data["blog"]
+    let avatar = data["avatar_url"]
+
+    /**
+     * getting required dom elements from document.
+     */
+    let imageElem = document.getElementById("avatar");
+    let bioElem = document.getElementById("bio");
+    let nameElem = document.getElementById("name");
+    let blogElem = document.getElementById("blog");
+    let locationElem = document.getElementById("location");
+
+    /**
+     * filling elements with data
+     */
+
+    imageElem.setAttribute("src", avatar);
+    imageElem.setAttribute("title", name + "'s Avatar");
+    imageElem.setAttribute("alt", name + "'s Avatar");
+
+    bioElem.innerHTML = bio ? bio.replace("\n", "<br>") : "";
+
+    nameElem.innerHTML = name;
+
+    if (blog) {
+        blogElem.setAttribute("href", blog);
+        blogElem.setAttribute("title", name + "'s blog address");
+        blogElem.innerHTML = blog;
+    }
+
+    locationElem.innerHTML = location;
+
+}
+
+
+function showError(message) {
+    let elem = document.getElementById("errorMessage")
+    elem.innerHTML = message;
+    elem.style.display = 'block';
+}
+
+
+function submitEventHandler(event) {
+
+    let username = document.getElementById("usernameInput").value;
+
+    if (!username && username.trim() == "") {
+        showError("invalid Username.")
+    } else {
+
+        getUserInfo(username).then(responseObj => {
+            if (responseObj.success) {
+                showDetail(responseObj.data);
+            } else {
+                showError(responseObj.errorMessage)
+            }
+        });
+    }
+
+    return false;
+}
+
+
+
+document.getElementById("formSubmitBtn").addEventListener("click", submitEventHandler);
