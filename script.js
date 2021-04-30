a48c6ac0697ea1bdf70009a694b767a8c68796ed
Ali Faraji <billa.svpc@gmail.com>
2021-04-30 19:27:00 +0430

    favoirte language based on 5 last pushed repos added.

--- script.js
+++ script.js
@@ -44,9 +44,13 @@ async function getUserInfo(username) {
         return response.json()
 
     }).then(function (body) {
-        responseObject.data = body
-        localStorage.setItem(username, JSON.stringify(responseObject)); //saving successful response on local storate
-        return responseObject;
+
+        return getFavoriteLanguage(body["repos_url"]).then(fav_lang => {
+            responseObject.data = body
+            responseObject.data["favorite_language"] = fav_lang;
+            localStorage.setItem(username, JSON.stringify(responseObject)); //saving successful response on local storate
+            return responseObject;
+        });
 
     }).catch(function (error) {
         responseObject.success = false;
@@ -54,11 +58,62 @@ async function getUserInfo(username) {
         return responseObject;
     })
 
-
-
 }
 
+async function getFavoriteLanguage(repositories_url) {
 
+    return fetch(repositories_url).then(responseHeader => {
+        if (!responseHeader.ok)
+            throw Error("Not Found!!");
+        return responseHeader.json();
+
+    }).then(repos => {
+
+        let numberOfCheckRepository = Math.min(repos.length, 5); // if repos count was less than 5
+
+        repos.sort((a, b) => {
+            return new Date(b["pushed_at"]) - new Date(a["pushed_at"]);
+        });
+
+
+        let promises = [];
+        repos.slice(0, numberOfCheckRepository).forEach(repo => {
+            promises.push(getLanguages(repo["languages_url"]));
+        });
+
+
+        let top_languages = {};
+        return Promise.all(promises).then(values => {
+            values.forEach(langs => {
+                if (langs) {
+                    Object.keys(langs).forEach(key => {
+                        if (!top_languages[key])
+                            top_languages[key] = 0;
+
+                        top_languages[key] += langs[key];
+                    });
+                }
+            });
+
+            //return language which has maximum score.
+            return Object.keys(top_languages).reduce((fav, item) => top_languages[item] > top_languages[fav] ? item : fav);
+        });
+
+    }).catch(error => {
+        return error.message;
+    });
+}
+
+async function getLanguages(languages_url) {
+
+    return fetch(languages_url).then(headerInfos => {
+        if (!headerInfos.ok) {
+            throw Error("Error In getting languages");
+        }
+        return headerInfos.json();
+    }).catch(_e => { });
+
+}
 
 function showDetail(data) {
 
@@ -68,8 +123,9 @@ function showDetail(data) {
     let name = data["name"];
     let bio = data["bio"];
     let location = data["location"];
-    let blog = data["blog"]
-    let avatar = data["avatar_url"]
+    let blog = data["blog"];
+    let avatar = data["avatar_url"];
+    let favLang = data["favorite_language"];
 
     /**
      * getting required dom elements from document.
@@ -79,6 +135,7 @@ function showDetail(data) {
     let nameElem = document.getElementById("name");
     let blogElem = document.getElementById("blog");
     let locationElem = document.getElementById("location");
+    let favLangElem = document.getElementById("favoriteLang");
 
     /**
      * filling elements with data
@@ -99,6 +156,8 @@ function showDetail(data) {
     }
 
     locationElem.innerHTML = location;
+
+    favLangElem.innerHTML = favLang;
 
 }
 
